16eaaac4acaf402f8e627e1559eb557b10298c9e
gracenho829 <gracenho829@gmail.com>
2022-11-03 09:22:42 -0400

    message

--- Lab 6/sender2.py
+++ Lab 6/sender2.py
@@ -2,9 +2,33 @@ import time
 import board
 import busio
 import adafruit_mpr121
+from adafruit_seesaw import seesaw, rotaryio, digitalio
 
 import paho.mqtt.client as mqtt
 import uuid
+
+# preparations for using the rotary encoder
+#
+#
+#
+seesaw = seesaw.Seesaw(board.I2C(), addr=0x36)
+
+seesaw_product = (seesaw.get_version() >> 16) & 0xFFFF
+print("Found product {}".format(seesaw_product))
+if seesaw_product != 4991:
+    print("Wrong firmware loaded?  Expected 4991")
+
+seesaw.pin_mode(24, seesaw.INPUT_PULLUP)
+button = digitalio.DigitalIO(seesaw, 24)
+button_held = False
+
+encoder = rotaryio.IncrementalEncoder(seesaw)
+last_position = None
+
+# setting up the client
+#
+#
+#
 
 client = mqtt.Client(str(uuid.uuid1()))
 client.tls_set()
@@ -21,9 +45,8 @@ i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
 while True:
-    for i in range(12):
-        if mpr121[i].value:
-        	val = f"Twizzler {i} touched!"
-        	print(val)
-        	client.publish(topic, val)
-    time.sleep(0.25)
+	position = -encoder.position
+	val = "Encoder Position" + str(position)
+	print(val)
+	client.publish(topic,val)
+	time.sleep(0.25)
